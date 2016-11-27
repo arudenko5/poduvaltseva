16237b1fbd8d38a44da523210a3a5c9da7f0a862
Rudenko Aleksey <Rudenko Aleksey>
2016-11-27 15:57:15 +0300

    base markup ready

--- source/js/main.js
+++ source/js/main.js
@@ -29,16 +29,37 @@
             dots: false
         });
 
-        /*$(".js-cases-next").on('click', function(){
-            console.log('next');
-            $cases.trigger('owl.next');
+    });
+})();
+
+(function(){
+    $(document).ready(function() {
+        var $map = $("#map");
+
+        ymaps.ready(function () {
+            var myMap = new ymaps.Map('map', {
+                    center: [55.761673306976135,37.62320879895021],
+                    zoom: 11,
+                    controls: []
+                }),
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                    hintContent: 'Юридическое бюро Аделины Подувальцевой',
+                    balloonContent: 'Юридическое бюро Аделины Подувальцевой'
+                }, {
+                    // Опции.
+                    // Необходимо указать данный тип макета.
+                    iconLayout: 'default#image',
+                    // Своё изображение иконки метки.
+                    iconImageHref: '../img/marker.png',
+                    // Размеры метки.
+                    iconImageSize: [41, 64],
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
+                    iconImageOffset: [-20, -64]
+                });
+            myMap.behaviors.disable('scrollZoom');
+            myMap.geoObjects.add(myPlacemark);
         });
-
-        $(".js-cases-prev").on('click', function(){
-            console.log('prev');
-            $cases.trigger('owl.prev');
-        });*/
-
     });
 })();
 
